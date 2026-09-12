8cf94e056131e861c9fead288e8a8f2894c4de01
Savro_s <suchkovstanislaw@yandex.ru>
2026-09-12 22:27:13 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, I'm Stanislaw Suchkov from group ИУ5-11/4!" << std::endl;
+    std::cout << "Hello from my C++ project!" << std::endl;
     return 0;
 }
